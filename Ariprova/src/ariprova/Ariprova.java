84ef3deea2e13a4c1b01fe3ace6c64a825343cbc
st116633 <cietto.valerio@educ.di.unito.it>
2014-06-06 16:23:55 +0200

    prima classe

--- Ariprova/src/ariprova/Ariprova.java
+++ Ariprova/src/ariprova/Ariprova.java
@@ -18,6 +18,7 @@ public class Ariprova {
     public static void main(String[] args) {
         // TODO code application logic here
         String ciaodaNetbeans= "";
+        int nothingToAdd;
     }
     
 }
